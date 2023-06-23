a4d8318239fd4bd051d4147c2bbf3ce431f995f6
enyanforh <ennyanforh@gmail.com>
2023-06-23 05:56:09 -0700

    great

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
 * main - Test function for positive or negative
-* Return: 0
+* Return: 0 (Successful)
 */
 int main(void)
 {
 	int i;
 
 	i = 0;
+
 	positive_or_negative(i);
 	return (0);
 }
