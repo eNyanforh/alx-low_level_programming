0dc2df84579dbf6c5b323205b84eb6c4c22f9d4b
enyanforh <ennyanforh@gmail.com>
2023-07-10 03:18:37 -0700

    work

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,23 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int _abs(int n);
+int _islower(int cny);
+int _isalpha(int ny);
+int _abs(int ny);
 int _isupper(int ny);
-int _isdigit(int c);
-int _strlen(char *s);
-void _puts(char *s);
+int _isdigit(int ny);
+int _strlen(char *ny);
+void _puts(char *ny);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+int _atoi(char *ny);
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
+char *_strncat(char *dest, char *src, int ny);
+char *_strncpy(char *dest, char *src, int ny);
+int _strcmp(char *n1, char *y2);
 char *_memset(char *ny1, char nb, unsigned int m);
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int ny);
 char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *ny, char *accept);
+char *_strpbrk(char *ny, char *accept);
 char *_strstr(char *haystack, char *needle);
 #endif
