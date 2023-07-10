d7de15d582cd961c68be88620cd8f8c4286881dc
enyanforh <ennyanforh@gmail.com>
2023-07-09 21:57:11 -0700

    multipl

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,43 +3,45 @@
 
 /**
  * _atoi - converts a string to an integer
- * @string: string to be converted
+ * @ny: string to be converted
+ * the two numbers and result of multiplication can be stored in integer
  * Return: the int converted from the string
  */
-int _atoi(char *string)
+
+int _atoi(char *ny)
 {
-	int j, e, m, len, g, digit;
+	int j, e, m, leng, f, digit;
 
 	j = 0;
 	e = 0;
 	m = 0;
-	len = 0;
-	g = 0;
+	leng = 0;
+	f = 0;
 	digit = 0;
 
-	while (string[len] != '\0')
-		len++;
+	while (ny[leng] != '\0')
+		leng++;
 
-	while (j < len && e == 0)
+	while (j < leng && f == 0)
 	{
-		if (string[j] == '-')
+		if (ny[j] == '-')
 			++e;
 
-		if (string[j] >= '0' && string[j] <= '9')
+		if (ny[j] >= '0' && ny[j] <= '9')
 		{
-			digit = string[j] - '0';
+			digit = ny[j] - '0';
 			if (e % 2)
 				digit = -digit;
 			m = m * 10 + digit;
-			g = 1;
-			if (string[j + 1] < '0' || string[j + 1] > '9')
+			f = 1;
+			if (ny[j + 1] < '0' || ny[j + 1] > '9')
 				break;
-			g = 0;
+			f = 0;
 		}
 		j++;
 	}
 
-	if (g == 0)
+	if (f == 0)
 		return (0);
 
 	return (m);
@@ -49,12 +51,12 @@ int _atoi(char *string)
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
+ * the program does not receive two arguments, program should print Error
  * Return: 0 (Success), 1 (Error)
  */
-
 int main(int argc, char *argv[])
 {
-	int ans, num1, num2;
+	int answer, num1, num2;
 
 	if (argc < 3 || argc > 3)
 	{
@@ -64,9 +66,9 @@ int main(int argc, char *argv[])
 
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
-	ans = num1 * num2;
+	answer = num1 * num2;
 
-	printf("%d\n", ans);
+	printf("%d\n", answer);
 
 	return (0);
 }
